dd9a70448de622db549fc659fb0af5b824a0c8cf
Chadi Benhaddou <73198190+ChadiBenhaddou@users.noreply.github.com>
2025-01-27 13:28:59 +0100

    Adding Symphony relation

--- enumchron_correction.sql
+++ enumchron_correction.sql
@@ -3,3 +3,7 @@
 SELECT i.barcode, i.itemcallnumber, i.enumchron 
 FROM items i join serialitems s on i.itemnumber = s.itemnumber JOIN serial s2 on s.serialid = s2.serialid 
 WHERE i.enumchron REGEXP '^ع\\. *\\([0-9]{4}\\)$';
+
+#synohony selection 
+select items.id, items.call_sequence, callnum.shelving_key, callnum.call_sequence, items.catalog_key, callnum.catalog_key from items, callnum 
+where items.catalog_key=callnum.catalog_key and items.call_sequence=callnum.call_sequence;
